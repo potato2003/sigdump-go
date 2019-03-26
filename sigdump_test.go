1c32e9fb0cb343fc0abfe082d9f13485eee07dc1
potato2003 <potato2003@gmail.com>
2019-03-26 16:56:26 +0900

    improve test coverage

--- sigdump_test.go
+++ sigdump_test.go
@@ -2,6 +2,8 @@ package sigdump
 
 import (
 	"bytes"
+	"fmt"
+	"os"
 	"strings"
 	"testing"
 )
@@ -17,5 +19,60 @@ func TestDumpGoroutine(t *testing.T) {
 	if !strings.Contains(actual, expected) {
 		t.Fatalf("bad: not contains %s", expected)
 		t.Fatalf("actual: %s", actual)
+	}
+}
+
+func TestOpenDumpPath(t *testing.T) {
+	// open Stdout
+	actual := openDumpPath("-")
+	expected := os.Stdout
+
+	if actual != expected {
+		t.Fatalf("bad: %+v", actual)
+	}
+
+	if "-" != DumpToStdout {
+		t.Fatalf("bad: DumpToStdout not equals to \"-\"")
+	}
+
+	// open Stderr
+	actual = openDumpPath("+")
+	expected = os.Stderr
+
+	if actual != expected {
+		t.Fatalf("bad: %+v", actual)
+	}
+
+	if "+" != DumpToStderr {
+		t.Fatalf("bad: DumpToStderr not equals to \"+\"")
+	}
+
+	// open file in /tmp dir
+	f := openDumpPath("")
+	f.Close()
+
+	path := fmt.Sprintf("/tmp/sigdump-%d.log", os.Getpid())
+	_, err := os.Stat(path)
+	if err != nil {
+		t.Fatalf("bad: faild to open dump file - %s", path)
+	}
+
+	// open specified file
+	path = fmt.Sprintf("/tmp/sigdump-test-%d.log", os.Getpid())
+	f = openDumpPath(path)
+	f.Close()
+
+	_, err = os.Stat(path)
+	if err != nil {
+		t.Fatalf("bad: faild to open dump file - %s", path)
+	}
+	os.Remove(path)
+
+	// open Stderr if file not exists
+	actual = openDumpPath("/tmp/not_found_dir/not_found_file")
+	expected = os.Stderr
+
+	if actual != expected {
+		t.Fatalf("bad: %+v", actual)
 	}
 }
